23fe4b59501a82e0fb7d9fd13a30f7cfc2034208
umair260 <cbsol026@gmail.com>
2023-07-11 17:29:37 +0500

    main cheerio

--- pages/api/load.js
+++ pages/api/load.js
@@ -1,18 +1,29 @@
 import { DirectoryLoader } from "langchain/document_loaders";
 import { TextLoader, PDFLoader } from "langchain/document_loaders";
 import path, { dirname } from 'path'
+import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
 
 
 
 export default async function (req, res) {
-  const dirName="/Users/umair/Documents/people";
+
+
+  // const dirName="/Users/umair/Downloads/tt";
   const arre=[];
-   const  loader = await new DirectoryLoader(dirName,{
-    ".txt": (path) => new TextLoader(path),
-     ".pdf": (path) => new PDFLoader(path, "text"),
-  });
-  const docsStored= await loader.load();
+  //  const  loader = await new DirectoryLoader(dirName,{
+  //   ".txt": (path) => new TextLoader(path),
+  //    ".pdf": (path) => new PDFLoader(path, "text"),
+  // });
+  // const docsStored= await loader.load();
+
+
+  const loader = new CheerioWebBaseLoader(
+    "https://esgdata.blockoffsets.com/feed/"
+  );
+  
+  const docsStored = await loader.load();
   console.log(docsStored)
+  
   let arr='';
   for (let i=0;i<docsStored.length;i++)
   {
